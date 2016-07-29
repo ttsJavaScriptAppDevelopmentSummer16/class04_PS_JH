c4e01b6c99e6d67bc21ba2717da789afb2ba2a9a
Ubuntu <ubuntu@ip-172-31-37-208.ec2.internal>
2016-07-29 00:53:50 +0000

    Adds final fiels for class 4 exercises

---
+++ scopeClosure.js
@@ -0,0 +1,22 @@
+
+function outer(){
+  var a = "string";
+  var b = {name1:"Priyam",name2:"Jude"};
+  console.log("This is the output from OUTER A " + a);
+  console.log("This is the output from OUTER B " + b);
+  function inner(a,b){
+    console.log("This is the output from INNER A before asignment " + a);
+    console.log("This is the output from INNER B before asignment " + b);
+    var a = "hello";
+    var b = {name1:"James",name2:"Kevin"};
+    console.log("This is the output from INNER A after asignment " + a);
+    console.log("This is the output from INNER B after asignment " + b);
+    b.name2 ="Amy"
+    console.log("This is the output from INNER B after asignment and property update " + b.name2);
+  }
+  inner(a,b);
+  console.log("This is the output from OUTER A after inner function " + a);
+  console.log("This is the output from OUTER A after inner function " + b);
+}
+
+outer();
---
+++ slideShow.js
@@ -0,0 +1,34 @@
+var slideShow = {
+  photoList: ["cat", "dog", "fish"],
+  currentPhotoIndex: 0,
+  nextPhoto:function (){
+    if (this.currentPhotoIndex < this.photoList.length) {
+      this.currentPhotoIndex +=1;
+    }else {
+      console.log("End of slideshow");
+    }
+  },
+  prevPhoto:function (){
+    if (this.currentPhotoIndex > 0) {
+      this.currentPhotoIndex -=1;
+    }else {
+      console.log("Start of slideshow");
+    }
+  },
+  getCurrentPhoto: function(){
+    return this.photoList[this.currentPhotoIndex];
+  }
+}
+
+for (i=0; i<5; i++){
+  rand = parseInt((Math.random()*2)%2)
+  console.log(rand);
+  if (rand===0){
+    slideShow.nextPhoto();
+  }else {
+    slideShow.prevPhoto();
+  }
+  console.log(slideShow.nextPhoto());
+  console.log(slideShow.currentPhotoIndex);
+  console.log(slideShow.getCurrentPhoto());
+}
